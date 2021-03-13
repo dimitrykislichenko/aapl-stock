f6c71091a1df462fc59eabd5f85618d9143082d6
Dimitry Kislichenko <dimitrykislichenko@gmail.com>
2021-03-13 19:26:00 +0200

    Add dataset for aapl stocks and an API

---
+++ src/api/api.ts
@@ -0,0 +1,67 @@
+import { add, Interval, isWithinInterval } from 'date-fns';
+import data from '../data/aapl';
+
+interface IData {
+  Date: number;
+  Open: number;
+  High: number;
+  Low: number;
+  Close: number;
+  Volume: number;
+  OpenInt: number;
+}
+
+// Last date of current dataset
+export const lastData = new Date(data[data.length - 1].Date);
+
+/**
+ * Fetch all available data
+ */
+export const getData = (): IData[] => data;
+
+/**
+ * Fetch data for specified date interval
+ */
+export const getByRange = (interval: Interval) => {
+  return data.filter((item) => isWithinInterval(new Date(item.Date), interval));
+};
+
+/**
+ * Fetch data for last week
+ */
+export const getWeek = () => {
+  getByRange({
+    start: add(lastData, { weeks: -1 }),
+    end: lastData,
+  });
+};
+
+/**
+ * Fetch data for last month
+ */
+export const getMonth = () => {
+  return getByRange({
+    start: add(lastData, { months: -1 }),
+    end: lastData,
+  });
+};
+
+/**
+ * Fetch data for last quarter
+ */
+export const getQuarter = () => {
+  return getByRange({
+    start: add(lastData, { months: -4 }),
+    end: lastData,
+  });
+};
+
+/**
+ * Fetch data for last year
+ */
+export const getYear = () => {
+  return getByRange({
+    start: add(lastData, { years: -1 }),
+    end: lastData,
+  });
+};
