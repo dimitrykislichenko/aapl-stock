7987593e7b8767f5ff618d625ec5bb209b1da474
Dimitry Kislichenko <dimitrykislichenko@gmail.com>
2021-03-14 05:21:45 +0200

    Add chart header

--- src/components/chart/Chart.tsx
+++ src/components/chart/Chart.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import {
   Area,
   AreaChart,
@@ -7,6 +8,7 @@ import {
   XAxis,
   YAxis,
 } from 'recharts';
+import { Header } from './Header';
 import {
   toLongDateFormat,
   toShortDateFormat,
@@ -15,47 +17,57 @@ import {
 import { IData } from '../../api/api';
 
 interface IChartProps {
+  header: string;
   data: (IData & { Value: number })[];
 }
 
-export const Chart: React.FC<IChartProps> = ({ data }) => (
-  <ResponsiveContainer width="100%" height="100%">
-    <AreaChart data={data}>
-      <XAxis
-        dataKey="Date"
-        tickFormatter={toShortDateFormat}
-        stroke="#FFFFFF"
-        tickLine={false}
-        axisLine={false}
-        interval="preserveEnd"
-        tickMargin={14}
-        minTickGap={80}
-      />
-      <YAxis
-        type="number"
-        domain={['dataMin', 'dataMax']}
-        tickFormatter={withCurrencySymbol}
-        stroke="#FFFFFF"
-        tickMargin={16}
-        orientation="left"
-        axisLine={false}
-        tickLine={false}
-        interval="preserveEnd"
-        minTickGap={80}
-        yAxisId={0}
-        width={80}
-      />
+const Wrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+`;
 
-      <Area
-        type="monotone"
-        strokeWidth={2}
-        dot={false}
-        dataKey="Value"
-        fill="#FE0F7D"
-        opacity="0.4"
-        stroke="#FE0F7D"
-      />
-      <Tooltip labelFormatter={toLongDateFormat} />
-    </AreaChart>
-  </ResponsiveContainer>
+export const Chart: React.FC<IChartProps> = ({ header, data }) => (
+  <Wrapper>
+    <Header>{header}</Header>
+    <ResponsiveContainer width="100%" height="100%">
+      <AreaChart data={data}>
+        <XAxis
+          dataKey="Date"
+          tickFormatter={toShortDateFormat}
+          stroke="#FFFFFF"
+          tickLine={false}
+          axisLine={false}
+          interval="preserveEnd"
+          tickMargin={14}
+          minTickGap={80}
+        />
+        <YAxis
+          type="number"
+          domain={['dataMin', 'dataMax']}
+          tickFormatter={withCurrencySymbol}
+          stroke="#FFFFFF"
+          tickMargin={16}
+          orientation="right"
+          axisLine={false}
+          tickLine={false}
+          interval="preserveEnd"
+          minTickGap={80}
+          yAxisId={0}
+          width={80}
+        />
+
+        <Area
+          type="monotone"
+          strokeWidth={2}
+          dot={false}
+          dataKey="Value"
+          fill="#FE0F7D"
+          opacity="0.4"
+          stroke="#FE0F7D"
+        />
+        <Tooltip labelFormatter={toLongDateFormat} />
+      </AreaChart>
+    </ResponsiveContainer>
+  </Wrapper>
 );
---
+++ src/components/chart/Header.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Header = styled.div`
+  position: absolute;
+  left: 5px;
+  width: 100%;
+  height: 70px;
+  padding: 8px;
+  font-size: 22px;
+  color: white;
+  text-align: left;
+  z-index: 10;
+  pointer-events: none;
+`;
